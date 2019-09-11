3f674b7d455607e8cd3c8dae978f001e6ae4e06a
League Student <league@iMac-37.attlocal.net>
2019-09-11 16:06:58 -0700

    :smile:

--- src/Module0Checkpoint.java
+++ src/Module0Checkpoint.java
@@ -1,33 +1,62 @@
+import java.applet.AudioClip;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Module0Checkpoint implements KeyListener{
-public static void main(String[] args) {
-	new Module0Checkpoint();
-	
-}
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Module0Checkpoint implements KeyListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
+	public static void main(String[] args) {
+		new Module0Checkpoint().run();
+
+	}
+
 	static void speak(String words) {
-	     try {
-	          Runtime.getRuntime().exec("say " + words).waitFor();
-	     } catch (Exception e) {
-	          e.printStackTrace();
-	          }
-	     }
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void run() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.addKeyListener(this);
+		
+		
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+	
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		int keyCode = e.getKeyCode();
+		if(keyCode == 38) {
+			speak("Ouch");
+		} 
+		else {
+			speak("No");
+		}
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
-	
-}
 
+	}
+
+}
